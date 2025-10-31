e2ee100d0fa6096509ea38e270d0aa8f2dac699b
FranciscoRossetto <fross2006@example.com>
2025-10-31 14:52:07 -0300

    JWT actualizado?

--- Backend_Node/src/controllers/userController.ts
+++ Backend_Node/src/controllers/userController.ts
@@ -3,7 +3,7 @@ import { User } from "../models/User";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || "secret123";
+const JWT_SECRET = process.env.JWT_SECRET || "clave_super_secreta";
 
 export const registerUser = async (req: Request, res: Response) => {
   try {
